a1c372f0c16bbdcc12dabd24b461c9ec22191231
pupilTong <pupiltong@github.com>
2022-01-13 05:53:38 -0800

    quick fix ts wrapper

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="node" />
-export declare function ether_ntoa(ip: string | Buffer): string;
+export declare function ether_ntoa(ip: Buffer): string;
 export declare function ether_aton(addr: string): Buffer;
