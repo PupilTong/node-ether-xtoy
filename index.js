bfc2b41c52f058f9d59f4decf9463f5ca3620734
pupilTong <pupiltong@github.com>
2022-01-13 21:59:37 -0800

    test Github action publish

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const addon = require('./build/Release/ether_xtoy');
+const addon = require('./build/Release/ether-xtoy');
 
 module.exports.ether_ntoa = addon.ether_ntoa;
 module.exports.ether_aton = addon.ether_aton;
